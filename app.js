c6246fa58905a683745d442f46f5d232f06d6005
Chirag Modi <cmodi@atlassoftweb.com>
2017-04-20 17:48:46 +0530

    req.connection error

---
+++ app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+var mysql = require('mysql'),
+    connection  = require('express-myconnection'),
+    dbOptions = {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      port: 3306,
+      database: 'nodejs'
+    };
+
+var index = require('./routes/index'),
+    customers = require('./routes/customers_route'),
+    users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/customers', customers);
+// app.use('/customers/add', customers.add);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+app.use(connection(mysql, dbOptions, 'request'));
+
+// app.use(
+//     connection(mysql,{
+//         host: 'localhost',
+//         user: 'root',
+//         password : '',
+//         port : 3307, //port mysql
+//         database:'nodejs'
+//     },'request')
+// );//route index, hello world
+
+
+module.exports = app;
